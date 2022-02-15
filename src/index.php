c04d866587906168e64d29d2f5c9d746eebaa6d3
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-15 14:28:01 +0530

    A program to calculate Electricity bill

--- src/index.php
+++ src/index.php
@@ -1,4 +1,28 @@
 <?php
+ if(isset($_GET)){
+   $unit = $_GET['unit'];
+ }
+ if($unit <= 50){
+    $total = $unit * 3.50;
+ }
+ elseif($unit>50 and $unit <=150){
+    $total = (($unit-50)*4.00) + (50*3.50);
+ }
+ elseif($unit>150 and $unit <=250){ 
+  $unit = $unit - 50;
+  $total = 50*3.50;
+  $unit= $unit -100 ;
+  $total += (100 *4.00) + ($unit * 5.20);   
+}
+else{
+  $unit = $unit - 50;
+  $total = 50*3.50;
+  $unit= $unit -100 ;
+  $total += (100 *4.00);
+  $unit = $unit -100;
+  $total += (100 *5.20) + ($unit * 6.50); 
+
+}
 
 ?>
 <!DOCTYPE html>
@@ -14,17 +38,19 @@
 
 <body>
   <div class="wrapper">
-    <form>
+    <h3> Program to calculate Electricity bill </h3>
+    <form method="GET" action="" >
       <div class="user">
         <dv class="col">
-          <label for="Name" class="row">Name</label>
+          <label for="Name" class="row">Unit</label>
         </dv>
         <div class="col">
-          <input type="text" id="Name" required>
+          <input type="text" id="unit" name ="unit" required>
         </div>
       </div>
-      <input type="submit" value="Submit" id="submit">
+      <input type="submit" value="Calculate" id="submit" name="submit">
     </form>
+    <H2>Total bill : Rs <?php echo $total?></h2>
   </div>
 </body>
 
