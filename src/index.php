1ea6dfe13045f980cb6695ba7a28eb4c8b278c56
Vikas Rathore <vikasrathore@cedcoss.com>
2022-02-15 13:08:17 +0530

    PHP-FORMS

---
+++ src/index.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>PHP-Forms</title>
+</head>
+
+<body>
+  <div class="wrapper">
+    <form>
+      <div class="user">
+        <dv class="col">
+          <label for="Name" class="row">Name</label>
+        </dv>
+        <div class="col">
+          <input type="text" id="Name" required>
+        </div>
+      </div>
+      <input type="submit" value="Submit" id="submit">
+    </form>
+  </div>
+</body>
+
+</html>
